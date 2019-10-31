e7fef8c11da734dcbe989848dc502e6d59f6ece5
Maxim Gladkov <contact@maximgladkov.com>
2019-11-01 00:35:10 +0200

    change naming

--- web/pages/p/[id].jsx
+++ web/pages/p/[id].jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import Link from 'next/link';
+import Link from "next/link";
 
-import firestore from '../../helpers/firebase';
+import firestore from "../../helpers/firestore";
 
 const getComponentByType = ({ type, settings }) => {
   switch (type) {
-    case ('text'):
+    case "text":
       return <p>{settings.text}</p>;
-    case ('list'):
+    case "list":
       return (
         <ul>
-          {settings.options.map((item) => <li key={item.id}>{item.label}</li>)}
+          {settings.options.map(item => (
+            <li key={item.id}>{item.label}</li>
+          ))}
         </ul>
       );
-    case ('image'):
+    case "image":
       return <img src={settings.src} alt={settings.alt} />;
     default:
       return null;
@@ -28,13 +30,13 @@ getComponentByType.propTypes = {
     text: PropTypes.string,
     options: PropTypes.array,
     src: PropTypes.string,
-    alt: PropTypes.string,
-  }),
+    alt: PropTypes.string
+  })
 };
 
 getComponentByType.defaultProps = {
-  type: '',
-  settings: {},
+  type: "",
+  settings: {}
 };
 
 const MessagePage = ({ content }) => {
@@ -55,37 +57,41 @@ const MessagePage = ({ content }) => {
       <Link href="/">
         <a>Go to all</a>
       </Link>
-      {content.map((block) => (
-        <div key={block.id}>
-          {getComponentByType(block)}
-        </div>
+      {content.map(block => (
+        <div key={block.id}>{getComponentByType(block)}</div>
       ))}
     </div>
   );
 };
 
 MessagePage.propTypes = {
-  content: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string,
-    content: PropTypes.array,
-  })),
+  content: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      content: PropTypes.array
+    })
+  )
 };
 
 MessagePage.defaultProps = {
-  content: null,
+  content: null
 };
 
-MessagePage.getInitialProps = async (context) => {
+MessagePage.getInitialProps = async context => {
   const { id } = context.query;
   let content;
 
-  await firestore.collection('pages').doc(id).get().then((doc) => {
-    const data = doc.data();
+  await firestore
+    .collection("pages")
+    .doc(id)
+    .get()
+    .then(doc => {
+      const data = doc.data();
 
-    if (data) {
-      content = data.content;
-    }
-  });
+      if (data) {
+        content = data.content;
+      }
+    });
 
   return { content };
 };
