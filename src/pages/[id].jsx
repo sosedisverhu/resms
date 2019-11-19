34d55b9609561956b3fd60cbe1d99e7adb321a96
Volodymyr Bilous <v.bilous.upwork@gmail.com>
2019-11-19 15:43:42 +0200

    Analytics added

--- src/pages/[id].jsx
+++ src/pages/[id].jsx
@@ -1,11 +1,16 @@
 import React, { useMemo } from 'react';
 import Head from 'next/head';
 
+import UAParser from 'ua-parser-js';
+import mapValues from 'lodash/mapValues';
+import isObject from 'lodash/isObject';
 import Router from '../routes';
 import RouteContext from '../contexts/RouteContext';
 import CampaingContext from '../contexts/CampaingContext';
 import DisplayCampaignBlocks from '../components/DisplayCampaignBlocks';
 import firebaseFromRequestWithFallback from '../helpers/firebase/firebaseFromRequestWithFallback';
+
+const serialize = (value) => (isObject(value) ? mapValues(value, serialize) : (value || ''));
 
 function CampaignPage({ route = {}, campaign = {} }) {
   const { title, image } = campaign;
@@ -39,11 +44,24 @@ CampaignPage.getInitialProps = async ({ req }) => {
 
   const firebase = firebaseFromRequestWithFallback(req);
   const firestore = firebase.firestore();
-
   const doc = await firestore
     .collection('campaigns')
     .doc(id)
     .get();
+
+  const { id: activityId } = firestore.collection('campaigns').doc(id).collection('activities').doc();
+
+  await firestore
+    .collection('campaigns')
+    .doc(id)
+    .collection('activities')
+    .doc(activityId)
+    .set({
+      trackedAt: +new Date(),
+      userAgent: req.headers['user-agent'],
+      ip: req.header('x-forwarded-for') || req.connection.remoteAddress,
+      ...mapValues(UAParser(req.headers['user-agent']), serialize),
+    });
 
   const campaign = { id: doc.id, ...doc.data() };
 
